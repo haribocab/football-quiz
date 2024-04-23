daccb4ca8ad21b69e30aa3b28305463d9b1ba8d8
Haruka Lange <haruka.lange@ds-extern.com>
2024-04-23 18:56:41 +0200

    remove country selector

--- client/src/App.js
+++ client/src/App.js
@@ -8,19 +8,18 @@ import PlayerDetail from "./pages/PlayerDetail";
 function App() {
   const [selectedYearId, setSelectedYearId] = useState(localStorage.getItem('selectedYearId') || '0');
   const [selectedTeamId, setSelectedTeamId] = useState(localStorage.getItem('selectedTeamId') || '0');
-  const [selectedCountryId, setSelectedCountryId] = useState(localStorage.getItem('selectedCountryId') || '0');
-  const [selectedLeagueIds, setSelectedLeagueIds] = useState([]);
+  const [selectedLeagueId, setSelectedLeagueId] = useState(localStorage.getItem('selectedLeagueId') || '0');
   const [selectedTeam, setSelectedTeam] = useState(localStorage.getItem('selectedTeam') ? JSON.parse(localStorage.getItem('selectedTeam')) : null);
   const [seasons, setSeasons] = useState([]);
-  const [year, setYear] = useState([]);
-  const [countries, setCountries] = useState([]);
+  const [selectedYear, setSelectedYear] = useState([]);
+  const [leagues, setLeagues] = useState([]);
   const [teams, setTeams] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY;
 
   const storeSelectedValues = () => {
     localStorage.setItem('selectedYearId', selectedYearId);
     localStorage.setItem('selectedTeamId', selectedTeamId);
-    localStorage.setItem('selectedCountryId', selectedCountryId);
+    localStorage.setItem('selectedLeagueId', selectedLeagueId);
     localStorage.setItem('selectedTeam', JSON.stringify(selectedTeam));
   };
 
@@ -30,10 +29,10 @@ function App() {
         const response = await fetch('./../seasons.json');
         const result = await response.json();
         setSeasons(result);
-        if(selectedYearId){
+        if(selectedYear){
           const matchSeason = result.find(season => season.year.id === parseInt(selectedYearId, 10));
-          setYear(matchSeason.year);
-          setCountries(matchSeason.countries);
+          setSelectedYear(matchSeason.year);
+          setLeagues(matchSeason.leagues);
         }
       } catch (error) {
         console.log("error", error);
@@ -44,6 +43,22 @@ function App() {
   }, []); 
 
   useEffect(() => {
+    // const fetchTeams = async () => {
+    //   try {
+    //     const response = await axios.get(`https://v3.football.api-sports.io/teams?season=${selectedYear.date}&league=${selectedLeagueId}`, {
+    //       headers: {
+    //         'x-rapidapi-key': `${apiKey}`,
+    //         'x-rapidapi-host': 'v3.football.api-sports.io'
+    //       }
+    //     });
+  
+    //     const result = response.data.response;
+    //     setTeams(result.map(team => team.team));
+    //   } catch (error) {
+    //     console.error("Error fetching players:", error);
+    //   }
+    // };
+
     const fetchTeams = async () => {
       try {
         var requestOptions = {
@@ -51,7 +66,6 @@ function App() {
           redirect: "follow",
         };
 
-        console.log(selectedLeagueIds);
 
         const response = await fetch("http://localhost:3031/response", requestOptions);
         const result = await response.json();
@@ -62,25 +76,23 @@ function App() {
     };
 
     fetchTeams();
-  }, [apiKey, selectedLeagueIds, selectedYearId]);
+  }, [apiKey, selectedYearId, selectedLeagueId]);
 
   useEffect(() => {
     storeSelectedValues();
-  }, [selectedYearId, selectedCountryId, selectedTeamId, selectedTeam]);
+  }, [selectedYearId, selectedLeagueId, selectedTeamId, selectedTeam]);
 
   const handleYearChange = (year) => {
     setSelectedYearId(year);
     const matchSeason = seasons.find(season => season.year.id === parseInt(year, 10));
-    setYear(matchSeason.year);
-    setCountries(matchSeason.countries);
-    setSelectedCountryId(0);
+    setSelectedYear(matchSeason.year);
+    setLeagues(matchSeason.leagues);
+    setSelectedLeagueId(0);
     setSelectedTeamId(0);
   };
 
-  const handleCountryChange = (countryId) => {
-    setSelectedCountryId(countryId);
-    const matchedCountry = countries.find(country => country.id === countryId);
-    setSelectedLeagueIds(matchedCountry.leagues.map(league => league.id));
+  const handleLeagueChange = (leagueId) => {
+    setSelectedLeagueId(leagueId);
     setSelectedTeamId(0);
   };
 
@@ -89,6 +101,8 @@ function App() {
     const team = teams.find(team => team.id === parseInt(teamId, 10));
     setSelectedTeam(team);
   };
+
+  {console.log(selectedLeagueId)}
 
   return (
     <div className="bg-gradient-to-r from-green-400 to-blue-500 min-h-screen">
@@ -109,14 +123,14 @@ function App() {
                 </select>
 
                 <select
-                  onChange={(e) => handleCountryChange(e.target.value)}
-                  value={selectedCountryId}
+                  onChange={(e) => handleLeagueChange(e.target.value)}
+                  value={selectedLeagueId}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   disabled={!selectedYearId}
                 >
-                  <option>Select Country</option>
-                  {countries.map(country => (
-                    <option key={country.id} value={country.id}>{country.name}</option>
+                  <option>Select League</option>
+                  {leagues.map(league => (
+                    <option key={league.id} value={league.id}>{league.name}</option>
                   ))}
                 </select>
 
@@ -124,7 +138,7 @@ function App() {
                   onChange={(e) => handleTeamChange(e.target.value)}
                   value={selectedTeamId}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  disabled={!selectedCountryId}
+                  disabled={!selectedLeagueId}
                 >
                   <option>Select Team</option>
                   {teams.map(team => (
@@ -137,7 +151,7 @@ function App() {
               </div>
             </div>
           } />
-          <Route path="/:selectedYearId/:selectedTeamId" element={selectedTeam && <Players team={{ season:year.name, country:selectedTeam.country, name:selectedTeam.name, logo:selectedTeam.logo}} />} />
+          <Route path="/:selectedYearId/:selectedTeamId" element={selectedTeam && <Players team={{ season:selectedYear.name, country:selectedTeam.country, name:selectedTeam.name, logo:selectedTeam.logo}} />} />
           <Route path="/player/:playerId" element={<PlayerDetail />} />
         </Routes>
       </Router>
